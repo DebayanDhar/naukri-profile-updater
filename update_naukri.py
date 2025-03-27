2e38250e8e89aa12f8554d801b668de1bd693b40
Debayan Dhar <debayandhar51@gmail.com>
2025-03-27 21:19:50 +0530

    imported packages

--- update_naukri.py
+++ update_naukri.py
@@ -3,6 +3,8 @@ import os
 import tempfile
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
